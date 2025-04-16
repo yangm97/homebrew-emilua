bd5e68be433ec6f8cf9ab80c493fd1a9f0028dd9
Yan Minari <yangm97@gmail.com>
2025-04-16 18:43:19 +0000

    update emilua to v0.11.3

--- Formula/emilua.rb
+++ Formula/emilua.rb
@@ -1,7 +1,7 @@
 class Emilua < Formula
   desc "Cross-platform LuaJIT engine with async IO and flexible threading"
   homepage "https://emilua.org/"
-  url "https://gitlab.com/emilua/emilua/-/archive/v0.11.2/emilua-v0.11.2.tar.gz"
+  url "https://gitlab.com/emilua/emilua/-/archive/v0.11.3/emilua-v0.11.3.tar.gz"
   sha256 "42c7ded56f0a0ede738c48b2303f850d4288ee0459c3753785de8799ed9f3476"
   license any_of: ["BSL-1.0", "MIT"]
   head "https://gitlab.com/emilua/emilua.git", branch: "emilua-0.11.x"
